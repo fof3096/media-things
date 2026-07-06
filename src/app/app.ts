5d6f0e8e7e6662049b96be542e7d4be024f432bb
Federico <federicoofarfan@gmail.com>
2026-07-05 22:26:09 -0300

    search-bar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { SearchBar } from './components/search-bar/search-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [SearchBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
